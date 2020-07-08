3f6bd74c0f125cd9e2353d226c89be405f0b8005
FelixvL <digitaleservice@gmail.com>
2020-07-08 15:19:19 +0200

    CL

--- Demo.java
+++ Demo.java
@@ -1,6 +1,11 @@
-class Demo{  // BL
+class Demo{  // CL
+	int a = 14;
 	final static public void main(String[] args){
 		System.out.println("We gaan beginnen");
+		new Demo().lopen(a);
+	}
+	void lopen(int c){
+		System.out.println(c);
 	}
 }
 
