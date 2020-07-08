9e16353899fb9845b18ff371edc5f2c1966725b7
FelixvL <digitaleservice@gmail.com>
2020-07-08 15:16:33 +0200

    BL

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
-class Demo{  // BT
-	final static static void main(String[] args){
+class Demo{  // BL
+	final static public void main(String[] args){
 		System.out.println("We gaan beginnen");
 	}
 }
