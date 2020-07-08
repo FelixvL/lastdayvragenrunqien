112143857d01cd07456cb3c9d4ae0d9553086405
FelixvL <digitaleservice@gmail.com>
2020-07-08 15:44:08 +0200

    HN

--- Demo.java
+++ Demo.java
@@ -1,10 +1,15 @@
-class Demo{  // CV
-	final static public void main(String[] args){
-		
-		new Demo().lopen();
+class Demo{  // HN
+	final static public void main(String[] args){		
+		new Middag().gaan(5);
 	}
-	void lopen(double... t){
-		System.out.println(""+t);
+}
+class Middag{
+	int a = 35;
+	void gaan(int a){
+		System.out.println("int"+a);
+	}
+	void gaan(byte a){
+		System.out.println("byte"+a);
 	}
 }
 
