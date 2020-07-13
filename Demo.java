08584382e1910003a8e80869dff54646f6a922e6
FelixvL <digitaleservice@gmail.com>
2020-07-13 15:36:44 +0200

    AT

--- Demo.java
+++ Demo.java
@@ -1,20 +1,25 @@
-class Demo{  // AD
+class Demo{  // AT
 	public static void main(String[] args){	
 		DRT drt = new MIN();
-		drt.a(drt.a);
-		System.out.println("wolkenachtig");
+		try{
+			drt.a(drt.a);
+			System.out.println("kan niet fout gaan");
+		}catch(Exception e){
+			System.out.println("wordt geprint");
+		}finally{
+			System.out.println("wordt niet geprint");
+		}
+		System.out.println("blauwe dag");
 	}
 	static DRT hoi(DRT drt){
 		return new NIM();
 	}
 }
-
 class NIM implements DRT{
 	int a = 2;
 	public void a(int a){
 		System.out.println("a"+a);
 	}   
-
 }
 class MIN extends NIM{
 	int a = 3;
