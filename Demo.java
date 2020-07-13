30b98a872ea5080b02eec999df79fab0e47cb6e4
FelixvL <digitaleservice@gmail.com>
2020-07-13 11:37:30 +0200

    BM

--- Demo.java
+++ Demo.java
@@ -1,17 +1,21 @@
-class Demo{  // BX
-
+class Demo{  // BM
 	public static void main(String[] args){	
 		Bot bot = new Bot();
+		bot.kracht = 8;
 		System.out.println( bot.bereken() );
 	}
 
 }
 
 class Bot{
-	int kracht = bereken();
+	static int kracht = 4;
 	public int bereken(){
-		System.out.println("bereken");
+		System.out.println("bereken"+this.kracht);
 		return 6;
+	}
+	Bot(String a){
+		this();
+		System.out.println("in Bot String");
 	}
 	Bot(){
 		System.out.println("in Bot");
