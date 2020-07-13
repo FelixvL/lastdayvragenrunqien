721b937f49dbb8e18566e34e49f945383a57f1ae
FelixvL <digitaleservice@gmail.com>
2020-07-13 13:56:59 +0200

    RF

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
-class Demo{  // RB
+class Demo{  // RF
 	public static void main(String[] args){	
-		int r = 0b1_0_1_1_0_1_0_0;   
+		float r = 0b1___0_1_1_0_1_0_0;   
 		System.out.println("gelukkig niet uitrekenen");
 
 	}
