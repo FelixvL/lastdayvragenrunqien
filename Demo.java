ed8990a69e1478ca3b28b8d550c63d54564ef9c9
FelixvL <digitaleservice@gmail.com>
2020-07-13 10:58:52 +0200

    nwe start

--- Demo.java
+++ Demo.java
@@ -1,19 +1,7 @@
 class Demo{  // OF
-	final static public void main(String[] args){	
-		new Middag();
-		new Avond();
-		System.out.println("Fiets");
-	}
-}
-class Middag extends Avond{
-	Middag Middag(){
-		System.out.println("middag gemaakt")
-	}
-}
-class Avond{
-	int a = 36;
-	Avond(){
-		System.out.println("avond gemaakt");
+	public static void main(String[] args){	
+		System.out.println("Start");
+		System.out.println("Stop");
 	}
 }
 
