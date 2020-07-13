e2d0ab1d8be36653ec441d6f4583df8e5c1556f8
FelixvL <digitaleservice@gmail.com>
2020-07-13 14:52:48 +0200

    IG

--- Demo.java
+++ Demo.java
@@ -1,16 +1,17 @@
-class Demo{  // IC
+class Demo{  // IG
 	public static void main(String[] args){	
 		hoi(null);
+		System.out.println("obrigado");
 	}
 	static DRT hoi(DRT drt){
 		return new NIM();
 	}
 }
 
-class NIM implements DRT{
+abstract class NIM implements DRT{   // Koe
 
 }
 
-interface DRT{
-
+interface DRT{    // Eetbaar
+	void lopen();
 }
