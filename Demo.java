01fe0c57edc4f914fbf28c4eaa60b75908107c9d
FelixvL <digitaleservice@gmail.com>
2020-07-13 11:01:55 +0200

    RT

--- Demo.java
+++ Demo.java
@@ -1,7 +1,19 @@
-class Demo{  // OF
+class Demo{  // RT
 	public static void main(String[] args){	
 		System.out.println("Start");
+		Bot bot = new Bot();
+		Bot bot2 = new Bot();
+		bot2.kracht = 4;
+		System.out.println(bot.kracht);
 		System.out.println("Stop");
+	}
+}
+
+class Bot{
+	static int kracht = bereken();
+	public int bereken(){
+		System.out.println("bereken");
+		return 6;
 	}
 }
 
