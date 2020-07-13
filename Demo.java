854a481c8c8931272d01ac5e77325f4de55ef1bc
FelixvL <digitaleservice@gmail.com>
2020-07-13 13:26:19 +0200

    ZA

--- Demo.java
+++ Demo.java
@@ -1,11 +1,18 @@
-class Demo{  // YU
+class Demo{  // ZA
 	public static void main(String[] args){	
-		System.out.println( new SSS(7).rrr );
+		int do = 34;
+		RRR ab = new RRR(do);
+		System.out.println(ab);
+
 	}
 }
 
 class RRR{
 	int sss = 4;
+	
+	RRR(){ 
+		System.out.println("hoi is rrr");
+	}
 	RRR(int a){
 		System.out.println("in SSS");
 	}
