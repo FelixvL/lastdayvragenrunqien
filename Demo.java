2782ed648b6304d4a43fb702d8a6388867ff6d24
FelixvL <digitaleservice@gmail.com>
2020-07-14 09:45:17 +0200

    BZ

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
-class Demo{  // BT
+class Demo{  // BZ
 	public static void main(String[] args){	
-		int[][][] arr = {null, {5,4,3},{{}}};
-		System.out.println("obrigada");
+		int[][][] arr = {null, {{5,4,3}},{{}}};
+		System.out.println("obrigada" + arr[2][0].length);
 	}
 }
