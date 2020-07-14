d74bdac443b9e4253fb87f8e1bf7489585fa796e
FelixvL <digitaleservice@gmail.com>
2020-07-14 12:01:36 +0200

    FJ

--- Demo.java
+++ Demo.java
@@ -1,8 +1,8 @@
-class Demo{  // FI
+class Demo{  // FJ
 	public static void main(String[] args){	
 		int a = 35;
-		if(a == 35)
-			if(a == 36)
+		if(a == 36)
+			if(a == 35)
 				System.out.println("q");
 		else
 			System.out.println("r");
