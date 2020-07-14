5ebafe43d804edcd1b0cfc231ccf80989ddb5797
FelixvL <digitaleservice@gmail.com>
2020-07-14 10:34:24 +0200

    VV

--- Demo.java
+++ Demo.java
@@ -1,14 +1,14 @@
-class Demo{  // VW
+class Demo{  // VV
 	public static void main(String[] args){	
-		Zwevend v = new ZweefVliegtuig();
-		System.out.println("Adieu");
+		Zwevend v = new Vliegtuig();
+		System.out.println("Au revoir");
 	}
 
 }
 interface Vliegend{
 
 }
-interface Zwevend{}
+interface Zwevend extends Vliegend{}
 class Vliegtuig implements Vliegend{
 
 }
