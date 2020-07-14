57ef630a6eee5500e06815e60003acf14db2752c
FelixvL <digitaleservice@gmail.com>
2020-07-14 11:47:45 +0200

    FD

--- Demo.java
+++ Demo.java
@@ -1,10 +1,9 @@
-class Demo{  // FC
+class Demo{  // FD
 	public static void main(String[] args){	
 		int a = 35;
 		if(a == 36)
 			System.out.println("ab");
 			System.out.println("cd");
-		else
 			System.out.println("ef");
 	}
 }
