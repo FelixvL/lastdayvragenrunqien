5efcd7fe7fcb45a1fa382f39a4f55c2fa35722cf
FelixvL <digitaleservice@gmail.com>
2020-07-14 12:06:27 +0200

    FK

--- Demo.java
+++ Demo.java
@@ -1,10 +1,11 @@
-class Demo{  // FJ
+class Demo{  // FK
 	public static void main(String[] args){	
 		int a = 35;
-		if(a == 36)
+		if(a == 35)
 			if(a == 35)
 				System.out.println("q");
 		else
+			throw new Error();
 			System.out.println("r");
 		System.out.println("s");
 	}
