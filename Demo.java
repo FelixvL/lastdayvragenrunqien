c2346f6b6daf908d37ffe31d20e9a56d8864faf8
FelixvL <digitaleservice@gmail.com>
2020-07-14 15:29:36 +0200

    UR

--- Demo.java
+++ Demo.java
@@ -1,18 +1,14 @@
-class Demo{  // FM
-	public static void main(String[] args){	
-		int a = 35;
-		try{
-		if(a == 35)
-			a = 37;
-			if(a == 36)
-				System.out.println("q");
-			else
-				throw new Error();
-		System.out.println("r");
-		System.out.println("s");
-		}finally{
-			System.out.println("go"+a);
-		}
-		System.out.println("no go "+ a);
+
+class Demo{    //   UR
+	public static void main(String[] args){
+		System.out.println("run");
 	}
 }
+class Fiets{
+	void fietsen(){
+		throw new ErroR();
+	}
+}
+
+
+class ErroR extends Exception{}
