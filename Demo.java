4348281b8c6dc937b0f352fd3a7d583a5c8af0bc
FelixvL <digitaleservice@gmail.com>
2020-07-14 15:45:34 +0200

    XE

--- Demo.java
+++ Demo.java
@@ -1,20 +1,24 @@
 
-class Demo{    //   UJ
+class Demo{    //   XE
 	public static void main(String[] args) throws Exception{
 		System.out.println("run");
 		try{
-		new Fiets().fietsen();
+			new Fiets().fietsen();
+			System.out.println("fiets op slot zetten");
 		}catch(FietsError fe){
 			System.out.println("genezen");
+		}catch(RaceFietsError rfe){
+			System.out.println("supersnel genezen");
 		}
 		System.out.println("Still Alive");
 	}
 }
 class Fiets{
 	void fietsen() throws Exception{
-		throw new FietsError();
+		throw new RaceFietsError();
 	}
 }
 
 
 class FietsError extends Exception{}
+class RaceFietsError extends Exception{}
