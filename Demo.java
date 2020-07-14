e4e3ef10ebc450abf02a9676df7d30fbbabcc3e4
FelixvL <digitaleservice@gmail.com>
2020-07-14 10:19:25 +0200

    LK

--- Demo.java
+++ Demo.java
@@ -1,14 +1,14 @@
-class Demo{  // LF
+class Demo{  // LK
 	public static void main(String[] args){	
 		Brief brief = new Brief();
 		brief.letter = new Letter("Y");
 		System.out.println(schrijven(brief, new Brief()));
 		System.out.println(brief.letter.letter);
 	}
-	static Letter schrijven(Brief brief, Brief feirb){
+	static String schrijven(Brief brief, Brief feirb){
 		feirb.letter = new Letter("Z");
 		brief = feirb;
-		return brief.letter;
+		return brief.letter.letter;
 	}
 }
 class Brief{
