7985604816b9786e3d853b37b64859adaf19d101
FelixvL <digitaleservice@gmail.com>
2020-07-14 12:00:44 +0200

    FI

--- Demo.java
+++ Demo.java
@@ -1,7 +1,11 @@
-class Demo{  // FG
+class Demo{  // FI
 	public static void main(String[] args){	
 		int a = 35;
-		char s = a == 36 ? 'r' : 'u';
-		System.out.println("ef"+s);
+		if(a == 35)
+			if(a == 36)
+				System.out.println("q");
+		else
+			System.out.println("r");
+		System.out.println("s");
 	}
 }
