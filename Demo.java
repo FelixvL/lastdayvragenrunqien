996667831a0898c99c2641dcdf1bb7cd15f20e40
FelixvL <digitaleservice@gmail.com>
2020-07-14 11:42:16 +0200

    FA

--- Demo.java
+++ Demo.java
@@ -1,26 +1,6 @@
-class Demo{  // VA
+class Demo{  // FA
 	public static void main(String[] args){	
-		Vliegtuig v = new ZweefVliegtuig();
-		System.out.println(v.wtf().nummer);
-	}
-
-}
-interface Vliegend extends Zwevend{
-	Zwevend wtf();
-}
-interface Zwevend{
-	String nummer = "Zwevend";
-}
-class Vliegtuig implements Vliegend{
-	String nummer =  "Vliegtuig";
-	public Vliegend wtf(){ 
-		System.out.println("Vliegtuig : "+ nummer);
-		return new Vliegtuig(); 
-	}
-}
-class ZweefVliegtuig extends Vliegtuig implements Vliegend, Zwevend{
-	public ZweefVliegtuig wtf(){
-		System.out.println("ZweefVliegtuig : "+ nummer);
-		return new ZweefVliegtuig();
+		for(int x = 0; x < 25; x++);
+		System.out.println("Adios");
 	}
 }
