d1523592165adbc732a59510015439eb842c48ae
FelixvL <digitaleservice@gmail.com>
2020-07-14 09:51:41 +0200

    WX

--- Demo.java
+++ Demo.java
@@ -1,6 +1,16 @@
-class Demo{  // BZ
+class Demo{  // WX
 	public static void main(String[] args){	
-		int[][][] arr = {null, {{5,4,3}},{{}}};
-		System.out.println("obrigada" + arr[2][0].length);
+		Brief brief = new Brief("R");
+		System.out.println(brief.letter.letter);
+	}
+}
+class Brief{
+	Letter letter;
+
+}
+class Letter{
+	String letter = "T";
+	Letter(String letter){
+		letter = letter;
 	}
 }
