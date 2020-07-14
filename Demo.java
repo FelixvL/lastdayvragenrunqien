9f34a0b39f6c8c40273236bb8cb7e74c48bf6063
FelixvL <digitaleservice@gmail.com>
2020-07-14 10:29:01 +0200

    VZ

--- Demo.java
+++ Demo.java
@@ -1,23 +1,18 @@
-class Demo{  // LK
+class Demo{  // VZ
 	public static void main(String[] args){	
-		Brief brief = new Brief();
-		brief.letter = new Letter("Y");
-		System.out.println(schrijven(brief, new Brief()));
-		System.out.println(brief.letter.letter);
+		Vliegend v = new ZweefVliegtuig();
 	}
-	static String schrijven(Brief brief, Brief feirb){
-		feirb.letter = new Letter("Z");
-		brief = feirb;
-		return brief.letter.letter;
-	}
-}
-class Brief{
-	Letter letter;
 
 }
-class Letter{
-	String letter = "R";
-	Letter(String letter){
-		this.letter = letter;
-	}
+interface Vliegend{
+
+}
+
+class Vliegtuig implements Vliegend{
+
+}
+
+
+class ZweefVliegtuig extends Vliegtuig{
+
 }
