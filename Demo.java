a8221d0fbcea208df603ca6a8ef9ec4054ae7853
FelixvL <digitaleservice@gmail.com>
2020-07-14 11:22:00 +0200

    VM

--- Demo.java
+++ Demo.java
@@ -1,24 +1,24 @@
-class Demo{  // VN
+class Demo{  // VM
 	public static void main(String[] args){	
 		Zwevend v = new Vliegtuig();
-		System.out.println("Doei");
+		System.out.println(v.nummer);
 	}
 
 }
 interface Vliegend extends Zwevend{
-	String nummer;
+	String nummer = "Vliegend";
 	Zwevend wtf();
 }
 interface Zwevend{
-	String nummer;
+	String nummer = "Zwevend";
 }
 class Vliegtuig implements Vliegend{
-	String nummer;
+	String nummer =  "Vliegtuig";
 	public ZweefVliegtuig wtf(){ return null; }
 }
 
 
 class ZweefVliegtuig extends Vliegtuig implements Vliegend, Zwevend{
-	String nummer;
+	String nummer =  "Zweefvliegtuig";
 	public ZweefVliegtuig wtf(){return null;}
 }
