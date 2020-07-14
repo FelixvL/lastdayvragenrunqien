bc40d3ffa6abb9d6f73d7547e33fa98a86724e83
FelixvL <digitaleservice@gmail.com>
2020-07-14 12:11:43 +0200

    FM

--- Demo.java
+++ Demo.java
@@ -1,12 +1,18 @@
-class Demo{  // FL
+class Demo{  // FM
 	public static void main(String[] args){	
 		int a = 35;
+		try{
 		if(a == 35)
+			a = 37;
 			if(a == 36)
 				System.out.println("q");
 			else
 				throw new Error();
 		System.out.println("r");
 		System.out.println("s");
+		}finally{
+			System.out.println("go"+a);
+		}
+		System.out.println("no go "+ a);
 	}
 }
