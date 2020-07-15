efdb3ff290fa4268d5c2589e405a93c6c9cfe23d
FelixvL <digitaleservice@gmail.com>
2020-07-15 09:23:02 +0200

    KL

--- Demo.java
+++ Demo.java
@@ -1,7 +1,28 @@
 
-class Demo{    //   KK
+class Demo{    //   KL
 	public static void main(String[] args){
-		float getal = new Float(40.5);
-		System.out.println("Bank");
+		Weegschaal ws = new Weegschaal();
+	
+		ws.wegen(  t -> t < 3   );
+
 	}
+}
+
+
+
+
+
+class Weegschaal{
+	void wegen(Voordelambda vdl){
+		boolean print = vdl.checken(5);
+		System.out.println("print: "+ print);
+	}
+}
+
+
+
+
+
+interface Voordelambda{
+	boolean checken(int vt);
 }
