da4620af00bcfd3771c78812ef27cf6cf8035c6c
FelixvL <digitaleservice@gmail.com>
2020-07-15 10:28:55 +0200

    KZ

--- Demo.java
+++ Demo.java
@@ -1,20 +1,20 @@
 
-class Demo{    //   KY
+class Demo{    //   KZ
 	public static void main(String[] args){
 		Weegschaal ws = new Weegschaal();
 		int getal = 25;
-		ws.wegen( y -> y.equals("of morgen") + "getal" );
+		ws.wegen( y -> y.charAt(3) + getal );
 		System.out.println("Brons");
 	}
 }
 
 class Weegschaal{
 	void wegen(Voordelambda vdl){
-		String print = vdl.checken("vandaag");
+		int print = vdl.checken("XXXXXXXXXXX");  //88
 		System.out.println("print: "+ print);
 	}
 }
 
 interface Voordelambda{
-	String checken(String hup);
+	int checken(String hup);
 }
