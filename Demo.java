7a625e22c28b63bcd1c681a4e7c50702369e79ff
FelixvL <digitaleservice@gmail.com>
2020-07-15 09:07:16 +0200

    donderdag

--- Demo.java
+++ Demo.java
@@ -1,32 +1,6 @@
 
 class Demo{    //   XM
 	public static void main(String[] args){
-		System.out.println("run");
-		try{
-			Fiets f = new RaceFiets();
-			f.fietsen();
-			System.out.println("fiets op slot zetten");
-		}catch(RaceFietsError rfe){
-			System.out.println("supersnel genezen");
-		}catch(FietsError fe){
-			System.out.println("genezen");
-		}
-		System.out.println("Still Alive");
-	}
-}
-class Fiets{
-	int snelheid = 23;
-	void fietsen() throws FietsError{
-		throw new RaceFietsError();
-	}
-}
-class RaceFiets extends Fiets{
-	int fietsen(int snelheid){
-		System.out.println("fietsen in peloton" + snelheid);
-		return snelheid;
-	}
-}
 
-
-class FietsError extends Exception{}
-class RaceFietsError extends FietsError{}
+	}
+}
