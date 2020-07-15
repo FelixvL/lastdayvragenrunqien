fb20cf8d048e2ec712fa7a482c49eaacd2ed8434
FelixvL <digitaleservice@gmail.com>
2020-07-15 10:33:39 +0200

    KA

--- Demo.java
+++ Demo.java
@@ -1,20 +1,20 @@
 
-class Demo{    //   KZ
+class Demo{    //   KA
 	public static void main(String[] args){
 		Weegschaal ws = new Weegschaal();
 		int getal = 25;
-		ws.wegen( y -> y.charAt(3) + getal );
+		ws.wegen( getal -> getal + getal );
 		System.out.println("Brons");
 	}
 }
 
 class Weegschaal{
 	void wegen(Voordelambda vdl){
-		int print = vdl.checken("XXXXXXXXXXX");  //88
+		int print = vdl.checken(20);  //88
 		System.out.println("print: "+ print);
 	}
 }
 
 interface Voordelambda{
-	int checken(String hup);
+	int checken(int hup);
 }
